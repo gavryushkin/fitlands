8963f13d8434ef0ea15a81aae0aa24a4eae0f568
Alex Gavryushkin <alex@gavruskin.com>
2016-05-28 12:24:59 +1200

    exact algorithm for epistsis (not tested) + descriptions

--- src/fitranks/three_way_epistasis.py
+++ src/fitranks/three_way_epistasis.py
@@ -20,6 +20,9 @@ def get_next_ordering(x):
     return [8, 7, 6, 5, 4, 3, 2, 1]
 
 
+# This function is for counting.
+# ordering is the order in which you choose elements from 1,...,8 to get the fitness ranking, e.g.:
+# ordering[4, 3, 1, 2, 6, 8, 5, 7] = [4, 3, 1, 1, 2, 3, 1, 1]
 def ordering_to_fitness(x):
     y = [1, 2, 3, 4, 5, 6, 7, 8]
     z = []
@@ -29,6 +32,50 @@ def ordering_to_fitness(x):
     return z
 
 
+# Returns i-th element of w which has + sign in the epistasis value.
+def epi_positives_get(i, w):
+    positives = {1, 5, 6, 7}
+    positives_fitness_ranks = []
+    for j in range(len(w)):
+        if w[j] in positives:
+            positives_fitness_ranks.append(j)
+    return positives_fitness_ranks[i]
+
+
+# Returns i-th element of w which has - sign in the epistasis value.
+def epi_negatives_get(i, w):
+    negatives = {2, 3, 4, 8}
+    negatives_fitness_ranks = []
+    for j in range(len(w)):
+        if w[j] in negatives:
+            negatives_fitness_ranks.append(j)
+    return negatives_fitness_ranks[i]
+
+
+# Returns true if fitness ranks w imply positive epistasis.
+def epistasis_positive(w):
+    for i in range(3):
+        if not epi_positives_get(i, w) >= epi_negatives_get(i, w):
+            return False
+    return True
+
+
+# Returns true if fitness ranks w imply negative epistasis.
+def epistasis_negative(w):
+    for i in range(3):
+        if not epi_positives_get(i, w) <= epi_negatives_get(i, w):
+            return False
+    return True
+
+
+# Returns true if fitness ranks w imply negative epistasis.
+def epistsis(w):
+    if epistasis_positive(w) or epistasis_negative(w):
+        return True
+    else:
+        return False
+
+
 def get_random_fitness_values():
     z = []
     for i in range(8):
@@ -36,8 +83,23 @@ def get_random_fitness_values():
     return sorted(z)
 
 
+def list_epistasis():
+    ordering = [1, 1, 1, 1, 1, 1, 1, 1]
+    fitness = [1, 2, 3, 4, 5, 6, 7, 8]
+    if epistsis(fitness):
+        print str(fitness)
+    while ordering != [8, 7, 6, 5, 4, 3, 2, 1]:
+        ordering = get_next_ordering(ordering)
+        fitness = ordering_to_fitness(ordering)
+        if epistsis(fitness):
+            print str(fitness)
+
+
+list_epistasis()
+
+
 # IMPORTANT: w_000 = w_1, w_001 = w_2, w_010 = w_3, w_100 = w_4, w_011 = w_5, w_101 = w_6, w_110 = w_7, w_111 = w_8
-def write_epistasis_to_file():
+def write_epistasis_to_file_random_algorithm():
     if not os.path.isfile("./epistasis_with_%s_cheks.txt" % N):
         epistasis_file = open("epistasis_with_%s_cheks.txt" % N, "w")
         epistasis_file.write("IMPORTANT: w_000 = w_1, w_001 = w_2, w_010 = w_3, w_100 = w_4, w_011 = w_5, w_101 = w_6, w_110 = w_7, w_111 = w_8\n")
@@ -75,4 +137,4 @@ def write_epistasis_to_file():
             i = 0
 
 
-write_epistasis_to_file()
+# write_epistasis_to_file()
