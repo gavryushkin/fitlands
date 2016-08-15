a67ebca153f9f16c7bad03d806b999d349c3c617
Alex Gavryushkin <alex@gavruskin.com>
2016-08-15 11:02:44 +0200

    compute the means of the fitness profiles

--- src/fitranks/models_max_likelyhood.py
+++ src/fitranks/models_max_likelyhood.py
@@ -1,6 +1,6 @@
 import random
 from models_HIV_2007 import datafile_hiv_process
-
+import numpy
 
 __author__ = '@gavruskin'
 
@@ -8,7 +8,7 @@ __author__ = '@gavruskin'
 # Given fitness profile (e.g. one returned by datafile_hiv_process),
 # generates n samples of pairwise comparisons of fitness.
 # The output is a matrix with i,j being the number of times i has higher fitness than j.
-# w_000 = w[0], w_001 = w[1], w_010 = w[2], w_100 = w[3], w_011 = w[4], w_101 = w[5], w_110 = w[6], w_111 = w[7]:
+# w_000 = w[0], w_001 = w[1], w_010 = w[2], w_100 = w[3], w_011 = w[4], w_101 = w[5], w_110 = w[6], w_111 = w[7]:^
 def sample_ranks_randomly(fit_data_list, n):
     output = [[0, 0, 0, 0, 0, 0, 0, 0],
               [0, 0, 0, 0, 0, 0, 0, 0],
@@ -32,6 +32,9 @@ def sample_ranks_randomly(fit_data_list, n):
 
 
 f = datafile_hiv_process()
+f_means = [numpy.mean(f[0]), numpy.mean(f[1]), numpy.mean(f[2]), numpy.mean(f[3]),
+           numpy.mean(f[4]), numpy.mean(f[5]), numpy.mean(f[6]), numpy.mean(f[7])]
+print f_means
 rankings = sample_ranks_randomly(f, 1000)
 for s in range(len(rankings)):
     print rankings[s]
