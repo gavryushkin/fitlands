2f7ec6620a6949b5c819a6adf6b3305aa6eb0fb7
devingreene <devin@greene.cz>
2016-08-15 15:05:54 -0400

    maxlik.R now has more informative error messages.  Commas removed from 'data2'

--- src/maxlik/maxlik.R
+++ src/maxlik/maxlik.R
@@ -1,15 +1,25 @@
 #!/usr/bin/Rscript 
+#options(digits=21)
 
 #Command line args used as initial point in optimization below
 argv=commandArgs(TRUE)
 argc=length(argv)
 N=argc+1 # number of elements
-if(N==1) q(status=1)
+if(N==1){
+	write("Error: No command line arguments?  Sure about that?",stderr())
+	q(status=1)
+}
 sqrt2=sqrt(2)
 
 #load the comparison data as a matrix
 file=file("stdin","r")
-M = matrix(scan(file,quiet=TRUE),ncol=N,nrow=N,byrow=TRUE)
+data = scan(file,quiet=TRUE)
+	# Correct number of command line arguments?
+if(length(data) != N*N){
+	write("Error: Command line arguments should be one less than number of genotypes",file=stderr())
+	q(status=2)
+}
+M = matrix(data,ncol=N,nrow=N,byrow=TRUE)
 close(file)
 
 #maximum likelihood function
@@ -35,7 +45,7 @@ Ini=as.numeric(argv)
 
 # print out _all_ warnings
 options(warn=1)
-estimate<-nlm(likfun,Ini,print.level=0)$estimate
+estimate=nlm(likfun,Ini,print.level=0)$estimate
 # print out parameters in first line of output
 parameters=c(0,estimate)
 cat("Parameters:",parameters,"\n")
@@ -44,7 +54,7 @@ rankings=c()
 
 #Sample size S.  Samples based on parameters derived from maximum likelihood.
 #Tallies proportion of each rank order which occurs.
-S=1000
+S=10000
 for(i in 1:S){
 	ranking=order(sapply(parameters,rnorm,n=1))-1
 	string=paste(ranking,collapse="<")
