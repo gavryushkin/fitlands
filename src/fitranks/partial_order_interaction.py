de08ae1137ffac7cfedb29239dd5747191b03c74
Alex Gavryushkin <alex@gavruskin.com>
2016-09-06 17:31:36 +0200

    all total extensions of partial orders taken from a file

--- src/fitranks/partial_order_interaction.py
+++ src/fitranks/partial_order_interaction.py
@@ -1,3 +1,4 @@
+import os.path
 from three_way_epistasis import get_next_ordering, ordering_to_fitness
 
 
@@ -6,7 +7,7 @@ __author__ = "@gavruskin"
 
 # Given a partial order in the form of adjacency lists, return all total extensions.
 # Loops through all total orders looking for compatible ones.
-def get_all_total_extensions_brute_force(graph):
+def all_total_extensions_brute_force(graph):
     output = []
     ordering = [1, 1, 1, 1, 1, 1, 1, 1]
     fitness = [1, 2, 3, 4, 5, 6, 7, 8]
@@ -27,4 +28,43 @@ def get_all_total_extensions_brute_force(graph):
                 break
         if compatible:
             output.append(fitness)
+    return output
+
+
+# Returns a list of partial orders on the set {1, ..., 8} given a file with partial orders on the set {000, ..., 111}.
+# The convention is: 000 = 1, 001 = 2, 010 = 3, 100 = 4, 011 = 5, 101 = 6, 110 = 7, 111 = 8
+# (To be compatible with other functions.)
+def partial_orders_from_file():
+    # if not os.path.isfile("./outputs/%s") % file_name:
+    #     print "Please put the file with partial orders into directory 'outputs' inside the working directory."
+    #     return
+    partial_orders_file = open("./outputs/partial_orders.txt", "r")
+    output = []
+    for line in partial_orders_file:
+        if line != "\n":
+            line = line.replace("[", "")
+            line = line.replace("]", "")
+            line = line.replace(" ", "")
+            partial_order = [int(s) for s in line.split(",")]
+            for i in range(len(partial_order)):
+                if partial_order[i] == 0:
+                    partial_order[i] = 1
+                if partial_order[i] == 1:
+                    partial_order[i] = 2
+                if partial_order[i] == 10:
+                    partial_order[i] = 3
+                if partial_order[i] == 100:
+                    partial_order[i] = 4
+                if partial_order[i] == 11:
+                    partial_order[i] = 5
+                if partial_order[i] == 101:
+                    partial_order[i] = 6
+                if partial_order[i] == 110:
+                    partial_order[i] = 7
+                if partial_order[i] == 111:
+                    partial_order[i] = 8
+            partial_order_formatted = []
+            for i in range(0, len(partial_order), 2):
+                partial_order_formatted.append([partial_order[i], partial_order[i+1]])
+            output.append(partial_order_formatted)
     return output
