f50b1148dba6525602dd6ec0c49a889ddbbf4574
Alex Gavryushkin <alex@gavruskin.com>
2016-09-07 09:58:58 +0200

    backup before the main function for analyzing p. orders

--- src/fitranks/partial_order_interaction.py
+++ src/fitranks/partial_order_interaction.py
@@ -34,11 +34,11 @@ def all_total_extensions_brute_force(graph):
 # Returns a list of partial orders on the set {1, ..., 8} given a file with partial orders on the set {000, ..., 111}.
 # The convention is: 000 = 1, 001 = 2, 010 = 3, 100 = 4, 011 = 5, 101 = 6, 110 = 7, 111 = 8
 # (To be compatible with other functions.)
-def partial_orders_from_file():
-    # if not os.path.isfile("./outputs/%s") % file_name:
-    #     print "Please put the file with partial orders into directory 'outputs' inside the working directory."
-    #     return
-    partial_orders_file = open("./outputs/partial_orders.txt", "r")
+def partial_orders_from_file(file_name):
+    if not os.path.isfile("./outputs/%s" % file_name):
+        print "Please put the file with partial orders into directory 'outputs' inside the working directory."
+        return
+    partial_orders_file = open("./outputs/%s" % file_name, "r")
     output = []
     for line in partial_orders_file:
         if line != "\n":
@@ -49,22 +49,34 @@ def partial_orders_from_file():
             for i in range(len(partial_order)):
                 if partial_order[i] == 0:
                     partial_order[i] = 1
-                if partial_order[i] == 1:
+                elif partial_order[i] == 1:
                     partial_order[i] = 2
-                if partial_order[i] == 10:
+                elif partial_order[i] == 10:
                     partial_order[i] = 3
-                if partial_order[i] == 100:
+                elif partial_order[i] == 100:
                     partial_order[i] = 4
-                if partial_order[i] == 11:
+                elif partial_order[i] == 11:
                     partial_order[i] = 5
-                if partial_order[i] == 101:
+                elif partial_order[i] == 101:
                     partial_order[i] = 6
-                if partial_order[i] == 110:
+                elif partial_order[i] == 110:
                     partial_order[i] = 7
-                if partial_order[i] == 111:
+                elif partial_order[i] == 111:
                     partial_order[i] = 8
             partial_order_formatted = []
             for i in range(0, len(partial_order), 2):
                 partial_order_formatted.append([partial_order[i], partial_order[i+1]])
             output.append(partial_order_formatted)
     return output
+
+
+def analyze_partial_orders(file_name):
+    partial_orders = partial_orders_from_file(file_name)
+    for partial_order in partial_orders:
+        total_extensions = all_total_extensions_brute_force(partial_order)
+        for total_extension in total_extensions:
+            print total_extension
+        print
+
+
+analyze_partial_orders("partial_orders.txt")
