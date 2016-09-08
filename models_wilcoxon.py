30755d7f7a6aac3b0766118982adc78a1ebe002f
Alex Gavryushkin <alex@gavruskin.com>
2016-09-08 15:43:00 +0200

    rank-sum test now returns the genotypes in the correct (not inversed) order

--- models_wilcoxon.py
+++ models_wilcoxon.py
@@ -1,18 +1,19 @@
 from models_HIV_2007 import datafile_hiv_process
+from partial_order_interaction import convert_to_genotype
 from scipy.stats import ranksums
-import numpy
 
 
 __author__ = "@gavruskin"
 
 
+# Returns the ordering of vectors of measurements according to Wilcoxon rank-sum test:
 def rank_sum(measurements):
     output = [1, 2, 3, 4, 5, 6, 7, 8]
     done = False
     while not done:
         done = True
         for i in range(len(measurements) - 1):
-            if ranksums(measurements[output[i] - 1], measurements[output[i + 1] - 1])[0] > 0:
+            if ranksums(measurements[output[i] - 1], measurements[output[i + 1] - 1])[0] < 0:
                 output[i], output[i+1] = output[i+1], output[i]
                 done = False
     for i in range(len(measurements) - 1):
@@ -20,10 +21,8 @@ def rank_sum(measurements):
     return output
 
 
+# Produce the ranking:
 f = datafile_hiv_process()
 rs = rank_sum(f)
 print
-print rs
-print
-for s in range(len(rs)):
-    print numpy.mean(f[rs[s] - 1])
+print convert_to_genotype(rs)
