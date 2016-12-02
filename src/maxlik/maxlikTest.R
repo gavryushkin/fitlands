47daf9b7edd80670538cdc5dfd9a2c35d3b19ad6
devingreene <devin@greene.cz>
2016-12-01 23:22:22 -0500

    Some files in my maxlik directory disappeared (even on my disk!). Thanks to git, I've snatched them from the grave.

---
+++ src/maxlik/maxlikTest.R
@@ -0,0 +1,18 @@
+#!/usr/bin/Rscript
+file=file("stdin","r")
+para=scan(file,quiet=TRUE)
+close(file)
+
+nelts=length(para)
+results=matrix(0,ncol=nelts,nrow=nelts)
+
+S=1000
+for(i in 1:S){
+	pair=sample(seq(para),size=2)
+	if(rnorm(1,para[pair[1]])>rnorm(1,para[pair[2]])) results[pair[1],pair[2]]=results[pair[1],pair[2]]+1 else
+	results[pair[2],pair[1]]=results[pair[2],pair[1]]+1
+}
+
+for(i in seq(nelts)){
+	cat(results[i,],"\n")
+}
