0f6b62b3469395565192a6534a00e2ca352a23e1
devingreene <devin@greene.cz>
2017-07-27 19:14:24 +0200

    Cleaned up badly written (by me) shell script

--- src/devinspictures/maketikzpicts.sh
+++ src/devinspictures/maketikzpicts.sh
@@ -1,11 +1,23 @@
-if cd matrices
+if [ -d all54 -a -d special20 ] && cd matrices
+
 then
 
-for f in mat[^s]*
-do mv `python ../maketikzpicts.py "$f"` ../all54
-done
+{
+	for f in mat[^s]*
+	do 
+		newf=`python ../maketikzpicts.py "$f"`
+		echo Making file $newf in directory all54
+    	/bin/mv $newf ../all54
+	done
+	
+	for f in mats*
+	do 
+		newf=`python ../maketikzpicts.py "$f"`
+		echo Making file $newf in directory special20
+		/bin/mv $newf ../special20
+	done
 
-for f in mats*
-do mv `python ../maketikzpicts.py "$f"` ../special20
-done
-fi
+} || cd ..
+fi 
+
+cd ..
